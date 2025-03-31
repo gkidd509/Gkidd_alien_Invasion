9854c7ded1cf566df236923b40b1d72ee83e7a72
RedBeard41 <42767289+RedBeard41@users.noreply.github.com>
2025-03-30 20:12:35 -0400

    Refactored Game Loop for event and drawing

--- alien_invasion.py
+++ alien_invasion.py
@@ -5,39 +5,46 @@ from ship import Ship
 
 class AlienInvasion:
 
-	def __init__(self):
-		pygame.init()
-		self.settings = Settings()
+    def __init__(self):
+        pygame.init()
+        self.settings = Settings()
 
-		self.screen = pygame.display.set_mode(
-			(self.settings.screen_w,self.settings.screen_h)
-			)
-		pygame.display.set_caption(self.settings.name)
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_w,self.settings.screen_h)
+            )
+        pygame.display.set_caption(self.settings.name)
 
-		self.bg = pygame.image.load(self.settings.bg_file)
-		self.bg = pygame.transform.scale(self.bg, 
-			(self.settings.screen_w, self.settings.screen_h)
-			)
+        self.bg = pygame.image.load(self.settings.bg_file)
+        self.bg = pygame.transform.scale(self.bg, 
+            (self.settings.screen_w, self.settings.screen_h)
+            )
 
-		self.running = True
-		self.clock = pygame.time.Clock()
+        self.running = True
+        self.clock = pygame.time.Clock()
 
-		self.ship = Ship(self)
-		
-	def run_game(self):
-		# Game loop 
-		while self.running:
-			for event in pygame.event.get():
-				if event.type == pygame.QUIT:
-					self.running = False
-					pygame.quit()
-					sys.exit()
+        self.ship = Ship(self)
+        
+    def run_game(self):
+        # Game loop 
+        while self.running:
+            self._check_events()
 
-			self.screen.blit(self.bg, (0,0))
-			self.ship.draw()
-			pygame.display.flip()
-			self.clock.tick(self.settings.FPS)
+            self._update_screen()
+            self.clock.tick(self.settings.FPS)
+
+    def _update_screen(self):
+        self.screen.blit(self.bg, (0,0))
+        self.ship.draw()
+        pygame.display.flip()
+
+    def _check_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
+                pygame.quit()
+                sys.exit()
+
 
 if __name__ == '__main__':
-	ai = AlienInvasion()
-	ai.run_game()
+    ai = AlienInvasion()
+    ai.run_game()
