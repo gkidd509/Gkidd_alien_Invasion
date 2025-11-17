16ac60d326bfdbd466d9c62c5c14a33b967348da
Gkidd509 <gkidd509@gmail.com>
2025-11-16 19:42:20 -0500

    Added ship movement via left and right arrow keys

--- alien_invasion.py
+++ alien_invasion.py
@@ -23,26 +23,45 @@ class AlienInvasion:
         self.ship = Ship(self)
 
 
-    def run_game(self):
+    def run_game(self) -> None:
         # Game Loop
         while self.running:
             self._check_events()
-
+            self.ship.update()
             self._update_screen()
             self.clock.tick(self.setting.FPS)
 
-    def _update_screen(self):
+    def _update_screen(self) -> None:
         self.screen.blit(self.bg, (0, 0))
         self.ship.draw()
         pygame.display.flip()
 
-    def _check_events(self):
+    def _check_events(self) -> None:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.running = False
                 pygame.quit()
                 sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event)
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_events(event)
 
+    def _check_keydown_events(self, event) -> None:
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right =True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+
+    def _check_keyup_events(self, event) -> None:
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
+        elif event.key == pygame.K_q:
+            self.running = False
+            pygame.quit()
+            sys.exit()
 
 if __name__ == '__main__':
     ai = AlienInvasion()
